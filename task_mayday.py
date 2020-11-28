2fcaeb959e7c53b6ad56e1c84a592622143f9850
dcadata <39072469+dcadata@users.noreply.github.com>
2020-11-27 20:04:24 -0500

    create directories exceptions and data

--- task_mayday.py
+++ task_mayday.py
@@ -6,11 +6,11 @@ def main():
     exception_text = ''
 
     try:
-        read_html('https://en.wikipedia.org/wiki/List_of_Mayday_episodes')[0].to_csv('mayday.csv')
+        read_html('https://en.wikipedia.org/wiki/List_of_Mayday_episodes')[0].to_csv('data/mayday.csv')
     except Exception as exc:
         exception_text = '\n\n'.join((str(exc), format_exc()))
 
-    open('exception.txt', 'w').write(exception_text)
+    open('exceptions/task_mayday.txt', 'w').write(exception_text)
 
 
 if __name__ == '__main__':
