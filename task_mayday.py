38332e95e611da215c9bf381bb5e1bad55246f1a
dcadata <39072469+dcadata@users.noreply.github.com>
2020-11-27 19:55:54 -0500

    update task_mayday.py

--- task_mayday.py
+++ task_mayday.py
@@ -1,16 +1,16 @@
-import pandas as pd
 from traceback import format_exc
+from pandas import read_html
 
 
 def main():
+    exception_text = ''
+
     try:
-        dfs = pd.read_html('https://en.wikipedia.org/wiki/List_of_Mayday_episodes')
-        dfs[0].to_csv('mayday.csv')
+        read_html('https://en.wikipedia.org/wiki/List_of_Mayday_episodes')[0].to_csv('mayday.csv')
     except Exception as exc:
-        text = '\n\n'.join((str(exc), format_exc()))
-        open('exception.txt', 'w').write(text)
-    else:
-        open('exception.txt', 'w').write('')
+        exception_text = '\n\n'.join((str(exc), format_exc()))
+
+    open('exception.txt', 'w').write(exception_text)
 
 
 if __name__ == '__main__':
