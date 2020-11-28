05bd8ca4d3b249dd82b438bb50fea96cbc321cd0
dcadata <39072469+dcadata@users.noreply.github.com>
2020-11-27 19:49:56 -0500

    add exception handling

--- task_mayday.py
+++ task_mayday.py
@@ -1,9 +1,14 @@
 import pandas as pd
+from traceback import format_exc
 
 
 def main():
-    dfs = pd.read_html('https://en.wikipedia.org/wiki/List_of_Mayday_episodes')
-    dfs[0].to_csv('mayday.csv')
+    try:
+        dfs = pd.read_html('https://en.wikipedia.org/wiki/List_of_Mayday_episodes')
+        dfs[0].to_csv('mayday.csv')
+    except Exception as exc:
+        text = '\n\n'.join((str(exc), format_exc()))
+        open('exception.txt', 'w').write(text)
 
 
 if __name__ == '__main__':
