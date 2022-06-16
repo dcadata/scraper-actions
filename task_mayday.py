54e9ffa3161c0da2e35ee1d51eb5c9b71b74be2b
Devon Ankar <devon.ankar@gmail.com>
2022-06-16 17:46:30 -0400

    Remove exceptions

--- task_mayday.py
+++ task_mayday.py
@@ -1,17 +1,4 @@
-from traceback import format_exc
 from pandas import read_html
 
-
-def main():
-    exception_text = ''
-
-    try:
-        read_html('https://en.wikipedia.org/wiki/List_of_Mayday_episodes')[0].to_csv('data/mayday.csv')
-    except Exception as exc:
-        exception_text = '\n\n'.join((str(exc), format_exc()))
-
-    open('exceptions/task_mayday.txt', 'w').write(exception_text)
-
-
 if __name__ == '__main__':
-    main()
+    read_html('https://en.wikipedia.org/wiki/List_of_Mayday_episodes')[0].to_csv('data/mayday.csv')
